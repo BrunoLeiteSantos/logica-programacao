1ce7ac04f14f11ddd3f13422cb37eade86f88a86
bruno leite <brunoleite42808@gmail.com>
2023-10-04 16:35:11 -0300

    Calculo de media

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-console.log("Ola, mundo!");
+// Logica de Programação;
+// Problema: obter tres notas de um aluno, calcular a média;
+// exibir a média e exibir a situação do aluno: aprovado;
+// em exame ou reprovado.
 
-document.write("Ola, documento");
+console.log("Inicio do programa");
 
-window.alert("Ola, Janela!");
+//declaração das variaveis
+const nota1= parseInt (prompt('Digite a nota 1'));
+const nota2= parseInt (prompt('Digite a nota 2'));
+const nota3= parseInt (prompt('Digite a nota 2'));
+
+// calculo da média
+const media= (nota1+nota2+nota3) /3;
+
+console.log('A nota 1 é:', nota1);
+console.log('A nota 2 é:', nota2);
+console.log('A nota 3 é:', nota3);
+
+console.log('A media do aluno é:' , media);
+
+console.log('Fim do programa');
