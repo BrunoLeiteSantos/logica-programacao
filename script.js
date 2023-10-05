34d36f838b815aa87ec4061592bb1fb29c1358aa
bruno leite <brunoleite42808@gmail.com>
2023-10-05 16:41:14 -0300

    Atividade de Logica da programação

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function mediaAluno () {
+
 // Logica de Programação;
 // Problema: obter tres notas de um aluno, calcular a média;
 // exibir a média e exibir a situação do aluno: aprovado;
@@ -6,9 +8,9 @@
 console.log("Inicio do programa");
 
 //declaração das variaveis
-const nota1= parseInt (prompt('Digite a nota 1'));
-const nota2= parseInt (prompt('Digite a nota 2'));
-const nota3= parseInt (prompt('Digite a nota 2'));
+const nota1= parseInt (document.getElementById('nota1').value);
+const nota2= parseInt (document.getElementById('nota2').value);
+const nota3= parseInt (document.getElementById('nota3').value);
 
 // calculo da média
 const media= (nota1+nota2+nota3) /3;
@@ -17,6 +19,17 @@ console.log('A nota 1 é:', nota1);
 console.log('A nota 2 é:', nota2);
 console.log('A nota 3 é:', nota3);
 
-console.log('A media do aluno é:' , media);
+document.getElementById('resultado').innerHTML = 'Aprovado=' + media;
+
+if (media >=7){
+    console.log ('APROVADO');
+} else if (media < 7 && media >=5 ) {
+    console.log ('EXAME');
+}else{
+    console.log ('REPROVADO');
+}
+
 
 console.log('Fim do programa');
+
+}
